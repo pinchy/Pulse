2547045a9ebc4df2d796966b130a65f82a816d56
pinchy <sebeckmas@gmail.com>
2022-04-16 22:21:34 +1000

    clang format

--- Pulse.cpp
+++ Pulse.cpp
@@ -1,17 +1,17 @@
 #include "Pulse.h"
 #include <Arduino.h>
 
-Pulse::Pulse(int p, int s, int i) : Output {p, s, i} {}
+Pulse::Pulse(int p, int s, int i) : Output{p, s, i} {}
 
 void Pulse::_setMode(Mode mode)
 {
-    if(this->_mode != mode)
+    if (this->_mode != mode)
     {
         this->_mode = mode;
         this->_count = 0;
         this->_lastInterval = millis();
 
-        if(this->_mode != PULSE_INACTIVE) this->on(); // start the mode by turning the led on
+        if (this->_mode != PULSE_INACTIVE) this->on();   // start the mode by turning the led on
     }
 }
 
@@ -35,63 +35,59 @@ void Pulse::timeout(uint32_t timeout, Mode modeOnTimeout)
 }
 
 
-
 void Pulse::tick()
 {
     switch (this->_mode)
     {
         case PULSE_BREATHE:
 
-            
 
-        break;
+            break;
 
         case PULSE_HEARTBEAT:
-            if(millis() - this->_lastInterval >= this->_heartbeat[this->_count])
-            {               
-                this->_lastInterval = millis();
-                if(++this->_count >= (sizeof(this->_heartbeat) / sizeof(this->_heartbeat[0]))) this->_count = 0;
-                this->set(!(this->_count % 2)); // even is on
-            }
-        break;
-        
-        case PULSE_FLASH:
-            if(millis() - this->_lastInterval >= this->_flash[this->_count])
+            if (millis() - this->_lastInterval >= this->_heartbeat[this->_count])
             {
                 this->_lastInterval = millis();
-                if(++this->_count >= (sizeof(this->_flash) / sizeof(this->_flash[0]))) this->_count = 0;
-                this->set(!(this->_count % 2)); // even is on
+                if (++this->_count >= (sizeof(this->_heartbeat) / sizeof(this->_heartbeat[0]))) this->_count = 0;
+                this->set(!(this->_count % 2));   // even is on
             }
-        break;
+            break;
+
+        case PULSE_FLASH:
+            if (millis() - this->_lastInterval >= this->_flash[this->_count])
+            {
+                this->_lastInterval = millis();
+                if (++this->_count >= (sizeof(this->_flash) / sizeof(this->_flash[0]))) this->_count = 0;
+                this->set(!(this->_count % 2));   // even is on
+            }
+            break;
 
         case PULSE_STROBE:
-            if(millis() - this->_lastInterval >= this->_strobe[this->_count])
+            if (millis() - this->_lastInterval >= this->_strobe[this->_count])
             {
                 this->_lastInterval = millis();
-                if(++this->_count >= (sizeof(this->_strobe) / sizeof(this->_strobe[0]))) this->_count = 0;
-                this->set(!(this->_count % 2)); // even is on
+                if (++this->_count >= (sizeof(this->_strobe) / sizeof(this->_strobe[0]))) this->_count = 0;
+                this->set(!(this->_count % 2));   // even is on
             }
-        break;
+            break;
 
         case PULSE_BLINK:
-            if(millis() - this->_lastInterval >= this->_blink[this->_count])
+            if (millis() - this->_lastInterval >= this->_blink[this->_count])
             {
                 this->_lastInterval = millis();
-                if(++this->_count >= (sizeof(this->_blink) / sizeof(this->_blink[0]))) this->_count = 0;
-                this->set(!(this->_count % 2)); // even is on
+                if (++this->_count >= (sizeof(this->_blink) / sizeof(this->_blink[0]))) this->_count = 0;
+                this->set(!(this->_count % 2));   // even is on
             }
-        break;
+            break;
 
         case PULSE_TIMEOUT:
-            if(millis() - this->_lastInterval >= this->_timeout)
+            if (millis() - this->_lastInterval >= this->_timeout)
             {
                 this->off();
                 this->_setMode(this->_modeOnTimeout);
             }
 
         case PULSE_INACTIVE:
-        break;
-
+            break;
     }
-
 }
--- Pulse.h
+++ Pulse.h
@@ -6,10 +6,10 @@
 #ifndef PULSE_H
 #define PULSE_H
 
-#include "Output.h"
 #include "Arduino.h"
+#include "Output.h"
 
-enum Mode {PULSE_INACTIVE, PULSE_BREATHE, PULSE_HEARTBEAT, PULSE_FLASH, PULSE_STROBE, PULSE_BLINK, PULSE_TIMEOUT};
+enum Mode { PULSE_INACTIVE, PULSE_BREATHE, PULSE_HEARTBEAT, PULSE_FLASH, PULSE_STROBE, PULSE_BLINK, PULSE_TIMEOUT };
 
 class Pulse : public Output
 {
@@ -19,7 +19,7 @@ class Pulse : public Output
 
         Mode _mode;
         Mode _modeOnTimeout;
-        uint8_t _count;    
+        uint8_t _count;
 
         // even elements are ON, odd are OFF. In ms
         uint16_t _heartbeat[4] = {20, 100, 20, 2000};
@@ -27,40 +27,38 @@ class Pulse : public Output
         uint16_t _flash[2] = {500, 500};
         uint16_t _blink[2] = {20, 5000};
 
-        void _setMode(Mode mode);           
+        void _setMode(Mode mode);
 
     public:
-    
-
         Pulse(int p, int s = LOW, int i = false);
 
-        /* 
+        /*
          * Breath the led, pulse in and out using PWM
          */
         void breathe();
 
-        /* 
+        /*
          * Flash the led like a heart-beat
          */
         void heartbeat();
 
-        /* 
+        /*
          * Flash the led continously
          */
         void flash();
 
-        /* 
+        /*
          * Strobe the led
          */
         void strobe();
-        
-        /* 
+
+        /*
          * Blink the led
          */
         void blink();
-        
 
-        /* 
+
+        /*
          * Call this every loop to update the pulse led
          */
         void tick();
@@ -75,7 +73,6 @@ class Pulse : public Output
             Turn on for timeout ms
         */
         void timeout(uint32_t timeout, Mode modeOnTimeout = PULSE_INACTIVE);
-        
-}; 
+};
 
 #endif
