f3387b55744171379a6bb44c2b5023bc3ca084bf
pinchy <sebeckmas@gmail.com>
2022-07-04 15:05:20 +1000

    clang format

--- Pulse.cpp
+++ Pulse.cpp
@@ -13,8 +13,7 @@ void Pulse::_setMode(Mode mode)
 
         this->_lastInterval = millis();
 
-        if (this->_mode != PULSE_INACTIVE)
-            this->on(); // start the mode by turning the led on
+        if (this->_mode != PULSE_INACTIVE) this->on();   // start the mode by turning the led on
     }
 }
 
@@ -41,58 +40,54 @@ void Pulse::tick()
 {
     switch (this->_mode)
     {
-    case PULSE_BREATHE:
+        case PULSE_BREATHE:
 
-        break;
+            break;
 
-    case PULSE_HEARTBEAT:
-        if (millis() - this->_lastInterval >= this->_heartbeat[this->_count])
-        {
-            this->_lastInterval = millis();
-            if (++this->_count >= (sizeof(this->_heartbeat) / sizeof(this->_heartbeat[0])))
-                this->_count = 0;
-            this->set(!(this->_count % 2)); // even is on
-        }
-        break;
+        case PULSE_HEARTBEAT:
+            if (millis() - this->_lastInterval >= this->_heartbeat[this->_count])
+            {
+                this->_lastInterval = millis();
+                if (++this->_count >= (sizeof(this->_heartbeat) / sizeof(this->_heartbeat[0]))) this->_count = 0;
+                this->set(!(this->_count % 2));   // even is on
+            }
+            break;
 
-    case PULSE_FLASH:
-        if (millis() - this->_lastInterval >= this->_flash[this->_count])
-        {
-            this->_lastInterval = millis();
-            if (++this->_count >= (sizeof(this->_flash) / sizeof(this->_flash[0])))
-                this->_count = 0;
-            this->set(!(this->_count % 2)); // even is on
-        }
-        break;
+        case PULSE_FLASH:
+            if (millis() - this->_lastInterval >= this->_flash[this->_count])
+            {
+                this->_lastInterval = millis();
+                if (++this->_count >= (sizeof(this->_flash) / sizeof(this->_flash[0]))) this->_count = 0;
+                this->set(!(this->_count % 2));   // even is on
+            }
+            break;
 
-    case PULSE_STROBE:
-        if (millis() - this->_lastInterval >= this->_strobe[this->_count])
-        {
-            this->_lastInterval = millis();
-            if (++this->_count >= (sizeof(this->_strobe) / sizeof(this->_strobe[0])))
-                this->_count = 0;
-            this->set(!(this->_count % 2)); // even is on
-        }
-        break;
+        case PULSE_STROBE:
+            if (millis() - this->_lastInterval >= this->_strobe[this->_count])
+            {
+                this->_lastInterval = millis();
+                if (++this->_count >= (sizeof(this->_strobe) / sizeof(this->_strobe[0]))) this->_count = 0;
+                this->set(!(this->_count % 2));   // even is on
+            }
+            break;
 
-    case PULSE_BLINK:
-        if (millis() - this->_lastInterval >= this->_blink[this->_count])
-        {
-            this->_lastInterval = millis();
-            if (++this->_count >= (sizeof(this->_blink) / sizeof(this->_blink[0])))
-                this->_count = 0;
-            this->set(!(this->_count % 2)); // even is on
-        }
-        break;
+        case PULSE_BLINK:
+            if (millis() - this->_lastInterval >= this->_blink[this->_count])
+            {
+                this->_lastInterval = millis();
+                if (++this->_count >= (sizeof(this->_blink) / sizeof(this->_blink[0]))) this->_count = 0;
+                this->set(!(this->_count % 2));   // even is on
+            }
+            break;
 
-    case PULSE_TIMEOUT:
-        if (millis() - this->_lastInterval >= this->_timeout)
-        {
-            this->off();
-            this->_setMode(this->_modeOnTimeout);
-        }
+        case PULSE_TIMEOUT:
+            if (millis() - this->_lastInterval >= this->_timeout)
+            {
+                this->off();
+                this->_setMode(this->_modeOnTimeout);
+            }
 
-    case PULSE_INACTIVE:
-        break;
+        case PULSE_INACTIVE:
+            break;
     }
 }
